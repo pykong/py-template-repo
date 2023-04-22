de1a9d34bbaa0c1d1a23edcca950808ee7e24e2e
Ben Felder <20211140+pykong@users.noreply.github.com>
2023-04-22 12:29:20 +0200

    Add loguru

--- src/app.py
+++ src/app.py
@@ -1,8 +1,10 @@
 import typer
+from loguru import logger
 
 app = typer.Typer()
 
 
+@logger.catch
 @app.command()
 def hello(name: str) -> None:
     """Greet user by name.
@@ -10,6 +12,7 @@ def hello(name: str) -> None:
     Args:
         name (str): The user's name.
     """
+    logger.debug(f"hello called with name:{name}")
     typer.echo(f"Hello, {name}!")
 
 
