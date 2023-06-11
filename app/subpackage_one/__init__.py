57d9b0463fb1534fda9d30879d4c1d403f70eb6c
Ben Felder <20211140+pykong@users.noreply.github.com>
2023-06-11 12:32:14 +0200

    Add experimental dynamic __all__ statement

--- app/subpackage_one/__init__.py
+++ app/subpackage_one/__init__.py
@@ -1,4 +1,4 @@
 from .module_1 import flip_coin
 from .module_2 import flip_n_coins
 
-__all__ = ["flip_coin", "flip_n_coins"]
+__all__ = [flip_coin.__name__, flip_n_coins.__name__]  # noqa: this is experimental
